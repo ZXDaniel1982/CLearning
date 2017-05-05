1b286d747ac948945322f5e7d82e9e7d2e9e79ee
dzhang <daniel.xuzhang@gmail.com>
2017-05-06 01:33:10 +1200

    Consistance and Variables

--- EmbeddedLinuxC_Learning/Lesson.c
+++ EmbeddedLinuxC_Learning/Lesson.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void)
 {
+    double pi = 3.1416;
+
 	printf("Hello world!\n");
+	printf("character: %c\ninteger: %d\nfloating point: %f\n", '}', 34, 3.14);
+	printf("sin(pi/2)=%f\nln1=%f\n", sin(pi/2), log(1.0));
 	return 0;
 }
