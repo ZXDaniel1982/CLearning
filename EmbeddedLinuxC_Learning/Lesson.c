5e1a510e5335b5a3d1632f6348f4f53115e52342
dzhang <daniel.xuzhang@gmail.com>
2017-05-14 11:52:30 +1200

    Mathematic functions fabs,pow,sqrt and a practical example to get Area from 2 points

--- EmbeddedLinuxC_Learning/Lesson.c
+++ EmbeddedLinuxC_Learning/Lesson.c
@@ -1,12 +1,48 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void)
+typedef struct Point
 {
-    double pi = 3.1416;
+	double x;
+	double y;
+}tPoint;
 
-	printf("Hello world!\n");
-	printf("character: %c\ninteger: %d\nfloating point: %f\n", '}', 34, 3.14);
-	printf("sin(pi/2)=%f\nln1=%f\n", sin(pi/2), log(1.0));
+const double pi = 3.1416;
+
+double getRadius(tPoint point1, tPoint point2)
+{
+	double radiusNum = 0;
+
+	radiusNum = sqrt(pow((point2.x - point1.x), 2) + pow((point2.y - point1.y), 2));
+	printf("radiusNum is %f\n", radiusNum);
+	return radiusNum;
+}
+
+double getArea(double radiusNum)
+{
+	double areaNum = 0;
+
+	areaNum = pi * pow(radiusNum, 2);
+	printf("areaNum is %f\n", areaNum);
+    return areaNum;
+}
+
+int main(int argc, char *argv[])
+{
+    float number = -112;
+    float sqrtnum = 121;
+    int printLen = 0;
+
+    tPoint point1 = {1, 2};
+    tPoint point2 = {4, 6};
+
+	printLen = printf("Hello world!\n");
+	printLen = printf("character: %c\ninteger: %d\nfloating point: %f, printLen = %d\n", '}', 34, 3.14, printLen);
+	printLen = printf("sin(pi/2)=%f\nln1=%f, printLen = %d\n", sin(pi/2), log(1.0), printLen);
+
+	printLen = printf("absolute of number is %f\n", fabs(number));
+	printLen = printf("sqrt of number is %f\n", sqrt(sqrtnum));
+
+	printLen = printf("Area of (1, 2) (4, 6) is %f\n", getArea(getRadius(point1, point2)));
 	return 0;
 }
