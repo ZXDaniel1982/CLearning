72b83482a820b6dcbb59ad689929299339eb7fee
ZXDaniel1982 <8142796+ZXDaniel1982@users.noreply.github.com>
2019-06-17 06:51:46 +0800

    Enable timer 5 interupt

--- STM32/STM32_Full_Keil/Core/Inc/stm32f1xx_it.h
+++ STM32/STM32_Full_Keil/Core/Inc/stm32f1xx_it.h
@@ -64,6 +64,7 @@ void TIM4_IRQHandler(void);
 void SPI1_IRQHandler(void);
 void USART1_IRQHandler(void);
 void EXTI15_10_IRQHandler(void);
+void TIM5_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
