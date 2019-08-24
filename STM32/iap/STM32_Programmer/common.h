1f837178de72b8dd262a96aa95760348e3b56553
ZXDaniel1982 <daniel.xuzhang@gmail.com>
2019-08-24 18:26:23 +1200

    Finish app

--- STM32/iap/STM32_Programmer/common.h
+++ STM32/iap/STM32_Programmer/common.h
@@ -5,6 +5,41 @@
  extern "C" {
 #endif
 
+typedef enum
+{
+  IAP_ERROR = 0,
+  IAP_SUCCESS,
+
+  IAP_ERROR_HEAD_INVALID,
+	
+	IAP_INIT_FAIL,
+	IAP_CMD_INIT,
+	IAP_INIT_SUCCESS,
+	
+	IAP_DEINIT_FAIL,
+	IAP_CMD_DEINIT,
+	IAP_DEINIT_SUCCESS,
+
+  IAP_ERASE_FAIL,
+  IAP_CMD_ERASE,
+  IAP_SUCCESS_ERASE,
+
+  IAP_STORE_FAIL,
+  IAP_STORE_BUSY,
+  IAP_SUCCESS_STORE,
+  IAP_CMD_STORE,
+
+  IAP_REBOOT_FAIL,
+  IAP_SUCCESS_REBOOT,
+  IAP_CMD_REBOOT,
+	
+	IAP_JUMP_FAIL,
+  IAP_SUCCESS_JUMP,
+  IAP_CMD_JUMP,
+
+  IAP_MAX_COMMAND = 100,
+} IAP_COMMANDS;
+
 int set_interface_attribs (int fd, int speed, int parity);
 void set_blocking (int fd, int should_block);
 
